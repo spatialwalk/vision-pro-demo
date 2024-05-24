ecf313f580273ab99e4e96f735f0d5c02a21f2da
yuhang <472617147@qq.com>
2024-05-24 14:22:14 +0800

    feat: init

---
+++ vision-pro-demo/vision_pro_demoApp.swift
@@ -0,0 +1,21 @@
+//
+//  vision_pro_demoApp.swift
+//  vision-pro-demo
+//
+//  Created by Yuhang on 2024/5/24.
+//
+
+import SwiftUI
+
+@main
+struct vision_pro_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
